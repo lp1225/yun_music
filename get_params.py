96cac05a80b603933d5d583e545cd4bf8bd981bf
lp <1402412786@qq.com>
2018-12-12 20:45:23 +0800

    网易云音乐热歌榜爬取,js解密

---
+++ get_params.py
@@ -0,0 +1,67 @@
+# coding = utf-8
+from Crypto.Cipher import AES
+import base64
+import requests
+
+first_param = '{rid:"", offset:"0", total:"true", limit:"80", csrf_token:""}'
+second_param = "010001"
+third_param = "00e0b509f6259df8642dbc35662901477df22677ec152b5ff68ace615bb7b725152b3ab17a876aea8a5aa76d2e417629ec4ee341f56135fccf695280104e0312ecbda92557c93870114af6c9d05c4f7f0c3685b7a46bee255932575cce10b424d813cfe4875d3e82047b97ddef52741d546b8e289dc6935b3ece0462db0a22b8e7"
+forth_param = "0CoJUm6Qyw8W8jud"
+
+
+def get_params():
+    iv = '0102030405060708'
+    first_key = forth_param
+    second_key = 16 * 'F'
+    h_encText = AES_encrypt(first_param, first_key, iv)
+    h_encText = AES_encrypt(h_encText, second_key, iv)
+    return h_encText
+
+
+def get_encSecKey():
+    encSecKey = "257348aecb5e556c066de214e531faadd1c55d814f9be95fd06d6bff9f4c7a41f831f6394d5a3fd2e3881736d94a02ca919d952872e7d0a50ebfa1769a7a62d512f5f1ca21aec60bc3819a9c3ffca5eca9a0dba6d6f7249b06f5965ecfff3695b54e1c28f3f624750ed39e7de08fc8493242e26dbc4484a01c76f739e135637c"
+    return encSecKey
+
+
+def AES_encrypt(text, key, iv):
+    if type(text) == type(b'123'):  # 这是判断当前变量的类型是bytes还是字符串，因为pycryptodome要
+        # 求参数要是字节类型
+
+        text = text.decode('utf-8')
+
+    pad = 16 - len(text) % 16  # 了解这个加密算法的人应该知道，加密的位数是16的位数，不够的话
+    # 进行不上，我也不了解这个算法，有兴趣的可以上网查查
+
+    text = text + pad * chr(pad)
+
+    iv = iv.encode('utf-8')
+    key = key.encode('utf-8')
+    encryptor = AES.new(key, AES.MODE_CBC, iv)
+    text = text.encode('utf-8')
+    encrypt_text = encryptor.encrypt(text)
+    encrypt_text = base64.b64encode(encrypt_text)
+    return encrypt_text
+
+
+if __name__ == "__main__":
+    params = get_params()
+    encSecKey = get_encSecKey()
+    print((params).decode('utf-8'))
+    print(encSecKey)
+
+    base_url = 'https://music.163.com/weapi/v1/resource/comments/R_SO_4_1332514589?csrf_token='
+
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36',
+        'Referer': 'https://music.163.com/',
+        'Host': 'music.163.com',
+        'Upgrade-Insecure-Requests': '1',
+    }
+    data = {
+        'params': params,
+        'encSecKey': encSecKey
+    }
+    response = requests.post(base_url, headers=headers, data=data)
+    print(response.text)
+    with open('xxx.json', 'w', encoding='utf-8') as f:
+        f.write(response.text)
---
+++ normal_spider.py
@@ -0,0 +1,70 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+import json
+
+
+class YunSpider:
+    """
+    网易云音乐热歌榜
+    """
+    def __init__(self):
+        self.base_url = 'https://music.163.com/discover/toplist?id=19723756'
+        self.headers = {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36',
+            'Referer': 'https://music.163.com/',
+            'Host': 'music.163.com',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+            'Upgrade-Insecure-Requests': '1',
+        }
+        self.temp = []
+
+    def send_request(self):
+        response = requests.get(self.base_url, headers=self.headers, timeout=60)
+        data = response.text
+
+        return data
+
+    def save_file(self, data):
+        with open('yun.json', 'w', encoding='utf-8') as f:
+            json.dump(data, f, sort_keys=False, indent=4, separators=(',', ': '), ensure_ascii=False)
+        print('==============+保存成功+=================')
+
+    def parse_html(self, html):
+        soup = BeautifulSoup(html)
+        res_json_list = soup.find('textarea', id='song-list-pre-data').get_text()
+        res_json_list = json.loads(res_json_list)
+        res_dict = {}
+        for res_json in res_json_list:
+            res_dict['id'] = res_json.get('album').get('id')
+            print('id', res_json.get('album').get('id'))
+
+            res_dict['music_name'] = res_json.get('album').get('name')
+            print('歌名:', res_json.get('album').get('name'))
+
+            res_dict['name'] = res_json.get('artists')[0].get('name')
+            print('歌手:', res_json.get('artists')[0].get('name'))
+
+            t1 = time.localtime(res_json.get('publishTime') / 1000)
+            t2 = time.strftime("%Y-%m-%d %H:%M:%S", t1)
+            res_dict['created_time'] = t2
+            print('歌曲发行时间:', t2)
+            t3 = res_json.get('duration') / 1000
+            min = int(t3 / 60)
+            sec = str(int(t3 % 60))
+            if len(sec) < 2:
+                sec = '0' + str(sec)
+            res_dict['time'] = str(min)+':'+sec
+            print('歌曲时长:{}:{}'.format(min, sec))
+            print('=============================================\n')
+            self.temp.append(res_dict)
+            res_dict = {}
+
+    def run(self):
+        html = self.send_request()
+        self.parse_html(html)
+        self.save_file(self.temp)
+
+
+if __name__ == '__main__':
+    YunSpider().run()
